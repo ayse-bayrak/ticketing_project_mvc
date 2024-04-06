47e417d583ee58d422f198782bc564a7f687f2b0
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-04-06 13:35:44 +0300

    Capture object from UI

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,7 +5,11 @@ import com.cydeo.service.UserService;
 import com.cydeo.service.impl.RoleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -33,9 +37,29 @@ public class UserController {
         //why UserDTO, let's look at the picture, which object is going to view
         //DTO is going to controller and then this DTO is going to pass from Controller
 
-        model.addAttribute("userList", userService.findAll());
+        model.addAttribute("users", userService.findAll());
 
         return "user/create";
+        //whatever inside that view any object, ant attribute you need to provide it inside this method
+        //because this method is returning this view
     }
+
+//    @GetMapping("/create1") //localhost:8080/user/create1
+//    public String createUser2(){
+//        //if i have different method which is returning same view, whatever we needs(objects, list of something)- you need provide it
+//        return "user/create";
+//    }
+@PostMapping("/create")
+public String insertUser(@ModelAttribute("user") UserDTO user, Model model){
+// whatever view that method is returning you need to go to that view (create html)
+        // you need to provides from this method whatever needs all attributes (user objects, roles, users)
+    model.addAttribute("user", new UserDTO());
+    model.addAttribute("roles", roleService.findAll());
+    userService.save(user);
+    model.addAttribute("users", userService.findAll());
+
+    return "user/create";
+}
+
 
 }
